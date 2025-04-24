2f870252139da7c9f1d9ee10767137f74c5c9b4f
Aruzhan Muratbek <aruzhan_1106@mail.ru>
2025-04-24 16:54:02 +0500

    entity classes created

--- src/main/java/org/example/Category.java
+++ src/main/java/org/example/Category.java
@@ -1,2 +1,22 @@
-package org.example;public class Category {
+package org.example;
+
+public class Category {
+    private int id;
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
--- src/main/java/org/example/Entry.java
+++ src/main/java/org/example/Entry.java
@@ -1,2 +1,51 @@
-package org.example;public class Entry {
+package org.example;
+
+import java.time.LocalDate;
+
+public class Entry {
+    private int id;
+    private String title;
+    private Category category;
+    private LocalDate dateLearned;
+    private LocalDate[] dateRecalled;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public LocalDate getDateLearned() {
+        return dateLearned;
+    }
+
+    public void setDateLearned(LocalDate dateLearned) {
+        this.dateLearned = dateLearned;
+    }
+
+    public LocalDate[] getDateRecalled() {
+        return dateRecalled;
+    }
+
+    public void setDateRecalled(LocalDate[] dateRecalled) {
+        this.dateRecalled = dateRecalled;
+    }
 }
